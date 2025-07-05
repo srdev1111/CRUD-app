344994e0968b96f0c7a59ad661797d2a79316769
srdev1111 <taylor.sct.robbins@gmail.com>
2025-07-05 16:49:35 -0500

    create CRUD

---
+++ src/components/App/index.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from '../Login';
+import Dashboard from '../Dashboard';
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    setIsAuthenticated(JSON.parse(localStorage.getItem('is_authenticated')));
+  }, []);
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Dashboard setIsAuthenticated={setIsAuthenticated} />
+      ) : (
+        <Login setIsAuthenticated={setIsAuthenticated} />
+      )}
+    </>
+  );
+};
+
+
+export default App;
